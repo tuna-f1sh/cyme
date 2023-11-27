c3fe6c15b1c160418c7b0d29815cf452c29f1edf
John Whittington <git@jbrengineering.co.uk>
2023-11-27 14:16:42 +0100

    still wip but class based descriptors

--- src/usb.rs
+++ src/usb.rs
@@ -132,6 +132,13 @@ impl TryFrom<f32> for Version {
     }
 }
 
+impl From<Version> for u16 {
+    fn from(v: Version) -> Self {
+        let Version(major, minor, sub_minor) = v;
+        ((major as u16) << 8) | ((minor as u16) << 4) | (sub_minor as u16)
+    }
+}
+
 /// Configuration attributes
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
 #[non_exhaustive]
@@ -850,9 +857,9 @@ pub struct USBEndpoint {
     pub max_packet_size: u16,
     /// Interval for polling endpoint data transfers. Value in frame counts. Ignored for Bulk & Control Endpoints. Isochronous must equal 1 and field may range from 1 to 255 for interrupt endpoints.
     pub interval: u8,
-    /// Extra descriptor data based on type
+    /// Extra descriptors data based on type
     #[serde(default)] // default for legacy json
-    pub extra: Option<DescriptorType>,
+    pub extra: Option<Vec<DescriptorType>>,
 }
 
 impl USBEndpoint {
@@ -916,9 +923,9 @@ pub struct USBInterface {
     /// Size of interface descriptor in bytes
     #[serde(default = "default_interface_desc_length")]
     pub length: u8,
-    /// Extra data for interface based on type
+    /// Extra descriptors for interface based on type
     #[serde(default)] // default for legacy json
-    pub extra: Option<DescriptorType>,
+    pub extra: Option<Vec<DescriptorType>>,
 }
 
 impl USBInterface {
@@ -973,9 +980,9 @@ pub struct USBConfiguration {
     /// Total length of configuration descriptor in bytes including all interfaces and endpoints
     #[serde(default)]
     pub total_length: u16,
-    /// Extra data for configuration based on type
+    /// Extra descriptors for configuration based on type
     #[serde(default)] // default for legacy json
-    pub extra: Option<DescriptorType>,
+    pub extra: Option<Vec<DescriptorType>>,
 }
 
 impl USBConfiguration {
@@ -1026,6 +1033,11 @@ pub struct USBDeviceExtra {
 #[allow(missing_docs)]
 // TODO structs for others
 pub enum DescriptorType {
+    Device(ClassDescriptor) = 0x01,
+    Config(ClassDescriptor) = 0x02,
+    String(String) = 0x03,
+    Interface(ClassDescriptor) = 0x04,
+    Endpoint(ClassDescriptor) = 0x05,
     DeviceQualifier = 0x06,
     OtherSpeedConfiguration = 0x07,
     Otg = 0x09,
@@ -1038,11 +1050,13 @@ pub enum DescriptorType {
     DeviceCapability = 0x10,
     WirelessEndpointCompanion = 0x11,
     WireAdaptor = 0x21,
-    RPipe = 0x22,
-    RcInterface = 0x23,
-    SsEndpointCompanion = 0x30,
+    // TODO use this in HidDescriptor vec
+    Report(HidReportDescriptor) = 0x22,
+    Physical = 0x23,
+    Hub = 0x29,
+    SuperSpeedHub = 0x2a,
+    SsEndpointCompanion(SsEndpointCompanionDescriptor) = 0x30,
     // these are internal
-    BaseClass(ClassDescriptor) = 0xfd,
     Unknown(Vec<u8>) = 0xfe,
     Junk(Vec<u8>) = 0xff,
 }
@@ -1064,24 +1078,53 @@ impl TryFrom<&[u8]> for DescriptorType {
         }
 
         match v[1] {
+            0x01 => Ok(DescriptorType::Device(ClassDescriptor::try_from(v)?)),
+            0x02 => Ok(DescriptorType::Config(ClassDescriptor::try_from(v)?)),
+            0x03 => Ok(DescriptorType::String(
+                String::from_utf8_lossy(v).to_string(),
+            )),
+            0x04 => Ok(DescriptorType::Interface(ClassDescriptor::try_from(v)?)),
+            0x05 => Ok(DescriptorType::Endpoint(ClassDescriptor::try_from(v)?)),
             0x06 => Ok(DescriptorType::DeviceQualifier),
             0x07 => Ok(DescriptorType::OtherSpeedConfiguration),
             0x09 => Ok(DescriptorType::Otg),
             0x0a => Ok(DescriptorType::Debug),
-            0x0b => Ok(DescriptorType::InterfaceAssociation(InterfaceAssociationDescriptor::try_from(
-                v,
-            )?)),
+            0x0b => Ok(DescriptorType::InterfaceAssociation(
+                InterfaceAssociationDescriptor::try_from(v)?,
+            )),
             0x0c => Ok(DescriptorType::Security(SecurityDescriptor::try_from(v)?)),
             0x0d => Ok(DescriptorType::Key),
-            0x0e => Ok(DescriptorType::Encrypted(EncryptionDescriptor::try_from(v)?)),
+            0x0e => Ok(DescriptorType::Encrypted(EncryptionDescriptor::try_from(
+                v,
+            )?)),
             0x0f => Ok(DescriptorType::Bos),
             0x10 => Ok(DescriptorType::DeviceCapability),
             0x11 => Ok(DescriptorType::WirelessEndpointCompanion),
             0x21 => Ok(DescriptorType::WireAdaptor),
-            0x22 => Ok(DescriptorType::RPipe),
-            0x23 => Ok(DescriptorType::RcInterface),
-            0x30 => Ok(DescriptorType::SsEndpointCompanion),
+            0x22 => Ok(DescriptorType::Report(HidReportDescriptor::try_from(v)?)),
+            0x23 => Ok(DescriptorType::Physical),
+            0x29 => Ok(DescriptorType::Hub),
+            0x2a => Ok(DescriptorType::SuperSpeedHub),
+            0x30 => Ok(DescriptorType::SsEndpointCompanion(
+                SsEndpointCompanionDescriptor::try_from(v)?,
+            )),
             _ => Ok(DescriptorType::Unknown(v.to_vec())),
+        }
+    }
+}
+
+impl DescriptorType {
+    /// Uses [`ClassCodeTriplet`] to update the [`ClassDescriptor`] with [`ClassCode`] for class specific descriptors
+    pub fn update_with_class_context<T: Into<ClassCode> + Copy>(
+        &mut self,
+        class_triplet: ClassCodeTriplet<T>,
+    ) -> Result<(), Error> {
+        match self {
+            DescriptorType::Device(d) => d.update_with_class_context(class_triplet),
+            DescriptorType::Config(c) => c.update_with_class_context(class_triplet),
+            DescriptorType::Interface(i) => i.update_with_class_context(class_triplet),
+            DescriptorType::Endpoint(e) => e.update_with_class_context(class_triplet),
+            _ => Ok(()),
         }
     }
 }
@@ -1149,6 +1192,36 @@ impl TryFrom<&[u8]> for InterfaceAssociationDescriptor {
     }
 }
 
+/// USB SS Endpoint Companion descriptor
+#[derive(Debug, Clone, Hash, PartialEq, Eq, Serialize, Deserialize)]
+#[allow(missing_docs)]
+pub struct SsEndpointCompanionDescriptor {
+    pub length: u8,
+    pub descriptor_type: u8,
+    pub max_burst: u8,
+    pub attributes: u8,
+}
+
+impl TryFrom<&[u8]> for SsEndpointCompanionDescriptor {
+    type Error = Error;
+
+    fn try_from(value: &[u8]) -> error::Result<Self> {
+        if value.len() < 6 {
+            return Err(Error::new(
+                ErrorKind::InvalidArg,
+                "SS Endpoint Companion descriptor too short",
+            ));
+        }
+
+        Ok(SsEndpointCompanionDescriptor {
+            length: value[0],
+            descriptor_type: value[1],
+            max_burst: value[2],
+            attributes: value[3],
+        })
+    }
+}
+
 /// USB security descriptor
 #[derive(Debug, Clone, Hash, PartialEq, Eq, Serialize, Deserialize)]
 #[allow(missing_docs)]
@@ -1189,7 +1262,7 @@ pub enum EncryptionType {
     Wired,
     Ccm1,
     Rsa1,
-    Reserved
+    Reserved,
 }
 
 impl From<u8> for EncryptionType {
@@ -1241,10 +1314,10 @@ impl TryFrom<&[u8]> for EncryptionDescriptor {
 pub enum ClassDescriptor {
     /// USB HID extra descriptor
     Hid(HidDescriptor),
-    /// USB Audio extra descriptor
-    Audio(GenericDescriptor),
-    /// Vendor defined extra descriptor
-    Vendor(GenericDescriptor),
+    /// Generic descriptor with Option<ClassCode>
+    ///
+    /// Used for most descriptors and allows for TryFrom without knowing the [`ClassCode`]
+    Generic(Option<ClassCodeTriplet<ClassCode>>, GenericDescriptor),
 }
 
 impl TryFrom<&[u8]> for ClassDescriptor {
@@ -1258,11 +1331,39 @@ impl TryFrom<&[u8]> for ClassDescriptor {
             ));
         }
 
-        match value[1] {
-            0x01 => Ok(ClassDescriptor::Audio(GenericDescriptor::try_from(value)?)),
-            0x03 => Ok(ClassDescriptor::Hid(HidDescriptor::try_from(value)?)),
-            _ => Ok(ClassDescriptor::Vendor(GenericDescriptor::try_from(value)?)),
+        Ok(ClassDescriptor::Generic(
+            None,
+            GenericDescriptor::try_from(value)?,
+        ))
+    }
+}
+
+impl From<ClassDescriptor> for Vec<u8> {
+    fn from(cd: ClassDescriptor) -> Self {
+        match cd {
+            ClassDescriptor::Generic(_, gd) => gd.into(),
+            ClassDescriptor::Hid(hd) => hd.into(),
         }
+    }
+}
+
+impl ClassDescriptor {
+    /// Uses [`ClassCodeTriplet`] to update the [`ClassDescriptor`] with [`ClassCode`] and descriptor if it is not [`GenericDescriptor`]
+    pub fn update_with_class_context<T: Into<ClassCode> + Copy>(
+        &mut self,
+        triplet: ClassCodeTriplet<T>,
+    ) -> Result<(), Error> {
+        match self {
+            ClassDescriptor::Generic(None, gd) => match (triplet.0.into(), triplet.1, triplet.2) {
+                (ClassCode::HID, _, _) => {
+                    *self = ClassDescriptor::Hid(HidDescriptor::try_from(gd.to_owned())?)
+                }
+                ct => *self = ClassDescriptor::Generic(Some(ct), gd.to_owned()),
+            },
+            _ => (),
+        }
+
+        Ok(())
     }
 }
 
@@ -1288,11 +1389,31 @@ impl TryFrom<&[u8]> for HidReportDescriptor {
             ));
         }
 
+        if value[0] != 0x22 {
+            return Err(Error::new(
+                ErrorKind::InvalidArg,
+                "HID report descriptor must have descriptor type 0x22",
+            ));
+        }
+
         Ok(HidReportDescriptor {
             descriptor_type: value[0],
             length: u16::from_le_bytes([value[2], value[1]]),
             data: value.get(3..).map(|d| d.to_vec()),
         })
+    }
+}
+
+impl From<HidReportDescriptor> for Vec<u8> {
+    fn from(hd: HidReportDescriptor) -> Self {
+        let mut ret = Vec::new();
+        ret.push(hd.descriptor_type);
+        ret.extend(hd.length.to_le_bytes());
+        if let Some(data) = hd.data {
+            ret.extend(data);
+        }
+
+        ret
     }
 }
 
@@ -1328,6 +1449,20 @@ impl TryFrom<&[u8]> for GenericDescriptor {
     }
 }
 
+impl From<GenericDescriptor> for Vec<u8> {
+    fn from(gd: GenericDescriptor) -> Self {
+        let mut ret = Vec::new();
+        ret.push(gd.length);
+        ret.push(gd.descriptor_type);
+        ret.push(gd.descriptor_subtype);
+        if let Some(data) = gd.data {
+            ret.extend(data);
+        }
+
+        ret
+    }
+}
+
 /// USB HID descriptor
 #[derive(Debug, Clone, Hash, PartialEq, Eq, Serialize, Deserialize)]
 #[allow(missing_docs)]
@@ -1360,6 +1495,30 @@ impl TryFrom<&[u8]> for HidDescriptor {
                 .map(|d| HidReportDescriptor::try_from(d))
                 .collect::<error::Result<Vec<_>>>()?,
         })
+    }
+}
+
+impl TryFrom<GenericDescriptor> for HidDescriptor {
+    type Error = Error;
+
+    fn try_from(gd: GenericDescriptor) -> error::Result<Self> {
+        let gd_vec: Vec<u8> = gd.into();
+        HidDescriptor::try_from(&gd_vec[..])
+    }
+}
+
+impl From<HidDescriptor> for Vec<u8> {
+    fn from(hd: HidDescriptor) -> Self {
+        let mut ret = Vec::new();
+        ret.push(hd.length);
+        ret.push(hd.descriptor_type);
+        ret.extend(u16::from(hd.bcd_hid).to_le_bytes());
+        ret.push(hd.country_code);
+        for desc in hd.descriptors {
+            ret.extend(Vec::<u8>::from(desc));
+        }
+
+        ret
     }
 }
 
