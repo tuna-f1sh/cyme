3a6665591ca68655e1fe57581f089c34a7ff11a6
John Whittington <git@jbrengineering.co.uk>
2025-02-09 17:05:53 +0100

    scolling support, use alternative term
    Scrolling support by buffering the DisplayWriter. Changed events so draw
    is separate from the device tree building - can scroll based on last
    buffer
    
    Might make impl into DisplayData so not passing PrintSettings around
    etc. Still want to track devices in buffer so that draw can show
    highlighted; change connected/disconnected style.

--- src/watch.rs
+++ src/watch.rs
@@ -1,13 +1,15 @@
 //! Watch for USB devices being connected and disconnected.
 use crossterm::{
     cursor,
-    event::{self, Event, KeyCode, KeyEvent, KeyModifiers},
-    execute, terminal,
+    event::{self, Event, KeyCode, KeyEvent, KeyModifiers, MouseEvent, MouseEventKind},
+    execute,
+    style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
+    terminal,
 };
 use std::io::stdout;
+use std::io::Write;
 use std::sync::{mpsc, Arc, Mutex};
 use std::thread;
-use std::time::Duration;
 
 use cyme::display::*;
 use cyme::error::{Error, ErrorKind, Result};
@@ -15,8 +17,18 @@ use cyme::profiler::{watch::SystemProfileStreamBuilder, Filter, SystemProfile};
 use futures_lite::stream::StreamExt;
 
 enum WatchEvent {
+    DrawDevices,
+    ScrollUp(usize),
+    ScrollDown(usize),
     Draw,
     Stop,
+}
+
+#[derive(Debug, Default)]
+struct DisplayData {
+    buffer: Vec<u8>,
+    max_offset: usize,
+    scroll_offset: usize,
 }
 
 pub fn watch_usb_devices(
@@ -34,15 +46,15 @@ pub fn watch_usb_devices(
     let mut stdout = stdout();
     execute!(
         stdout,
+        // alternative screen like other terminal apps so main screen buffer isn't cleared
+        terminal::EnterAlternateScreen,
         cursor::Hide,
         terminal::Clear(terminal::ClearType::All)
     )
     .map_err(|e| Error::new(ErrorKind::Other("crossterm"), &e.to_string()))?;
 
     terminal::enable_raw_mode()?;
-
-    // first draw
-    draw_devices(&spusb, &print_settings)?;
+    let mut display_data = DisplayData::default();
 
     // pass spusb to stream builder, will get Arc<Mutex<SystemProfile>> back below
     let mut profile_stream = SystemProfileStreamBuilder::new()
@@ -57,27 +69,39 @@ pub fn watch_usb_devices(
     // get a reference to the SystemProfile now since profile_stream can't be moved
     // main thread needs to draw with SystemProfile outside of the stream
     let spusb = profile_stream.get_profile();
+    // first draw
+    tx.send(WatchEvent::DrawDevices).unwrap();
 
     let print_settings_clone = Arc::clone(&print_settings);
 
+    // Thread to listen for USB profiling events
     let tx_clone = tx.clone();
     thread::spawn(move || {
         futures_lite::future::block_on(async {
             while profile_stream.next().await.is_some() {
                 // local spusb Arc<Mutex<SystemProfile>> is updated since we have a reference to it
-                tx_clone.send(WatchEvent::Draw).unwrap();
+                tx_clone.send(WatchEvent::DrawDevices).unwrap();
             }
         });
     });
 
-    // Thread to listen for keyboard events
+    // Thread to listen for terminal events
     thread::spawn(move || loop {
-        if event::poll(Duration::from_millis(100)).unwrap() {
-            if let Event::Key(KeyEvent {
+        match event::read().unwrap() {
+            Event::Resize(_, _) => {
+                tx.send(WatchEvent::DrawDevices).unwrap();
+            }
+            Event::Mouse(MouseEvent { kind, .. }) => {
+                if kind == MouseEventKind::ScrollUp {
+                    tx.send(WatchEvent::ScrollUp(1)).unwrap();
+                } else if kind == MouseEventKind::ScrollDown {
+                    tx.send(WatchEvent::ScrollDown(1)).unwrap();
+                }
+            }
+            Event::Key(KeyEvent {
                 code, modifiers, ..
-            }) = event::read().unwrap()
-            {
-                if match (code, modifiers) {
+            }) => {
+                match (code, modifiers) {
                     (KeyCode::Char('q'), _)
                     | (KeyCode::Esc, _)
                     | (KeyCode::Char('c'), KeyModifiers::CONTROL) => {
@@ -87,47 +111,68 @@ pub fn watch_usb_devices(
                     (KeyCode::Char('v'), _) => {
                         let mut print_settings = print_settings.lock().unwrap();
                         print_settings.verbosity = (print_settings.verbosity + 1) % 4;
-                        true
+                        tx.send(WatchEvent::DrawDevices).unwrap();
                     }
                     (KeyCode::Char('t'), _) => {
                         let mut print_settings = print_settings.lock().unwrap();
                         print_settings.tree = !print_settings.tree;
-                        true
+                        tx.send(WatchEvent::DrawDevices).unwrap();
                     }
                     (KeyCode::Char('h'), _) => {
                         let mut print_settings = print_settings.lock().unwrap();
                         print_settings.headings = !print_settings.headings;
-                        true
+                        tx.send(WatchEvent::DrawDevices).unwrap();
                     }
                     (KeyCode::Char('m'), _) => {
                         let mut print_settings = print_settings.lock().unwrap();
                         print_settings.more = !print_settings.more;
-                        true
+                        tx.send(WatchEvent::DrawDevices).unwrap();
                     }
                     (KeyCode::Char('d'), _) => {
                         let mut print_settings = print_settings.lock().unwrap();
                         print_settings.decimal = !print_settings.decimal;
-                        true
+                        tx.send(WatchEvent::DrawDevices).unwrap();
+                    }
+                    (KeyCode::Char('j'), _) | (KeyCode::PageDown, _) => {
+                        tx.send(WatchEvent::ScrollDown(1)).unwrap();
+                        tx.send(WatchEvent::Draw).unwrap();
+                    }
+                    (KeyCode::Char('k'), _) | (KeyCode::PageUp, _) => {
+                        tx.send(WatchEvent::ScrollUp(1)).unwrap();
+                        tx.send(WatchEvent::Draw).unwrap();
                     }
                     // TODO:
                     // filter pop-up enter filter - / like vim?
                     // sort pop-up enter sort
-                    _ => false,
-                } {
-                    tx.send(WatchEvent::Draw).unwrap();
-                }
+                    _ => (),
+                };
             }
+            _ => {}
         }
     });
 
     // Main event loop
+    // manages the display and listens for events
     loop {
         match rx.recv() {
-            Ok(WatchEvent::Draw) => {
+            Ok(WatchEvent::ScrollUp(n)) => {
+                display_data.scroll_offset = display_data.scroll_offset.saturating_sub(n);
+            }
+            Ok(WatchEvent::ScrollDown(n)) => {
+                display_data.scroll_offset = display_data
+                    .max_offset
+                    .min(display_data.scroll_offset.saturating_add(n));
+            }
+            Ok(WatchEvent::DrawDevices) => {
                 let print_settings = print_settings_clone.lock().unwrap();
                 let mut spusb = spusb.lock().unwrap();
                 cyme::display::prepare(&mut spusb, &filter.lock().unwrap(), &print_settings);
-                draw_devices(&spusb, &print_settings)?;
+                draw_devices_buffered(&spusb, &print_settings, &mut display_data)?;
+                draw(&mut display_data, &print_settings)?;
+            }
+            Ok(WatchEvent::Draw) => {
+                let print_settings = print_settings_clone.lock().unwrap();
+                draw(&mut display_data, &print_settings)?;
             }
             Ok(WatchEvent::Stop) => {
                 break;
@@ -138,16 +183,71 @@ pub fn watch_usb_devices(
         }
     }
 
-    execute!(stdout, cursor::Show)
-        .map_err(|e| Error::new(ErrorKind::Other("crossterm"), &e.to_string()))?;
+    execute!(
+        stdout,
+        terminal::Clear(terminal::ClearType::All),
+        terminal::LeaveAlternateScreen,
+        cursor::Show,
+    )
+    .map_err(|e| Error::new(ErrorKind::Other("crossterm"), &e.to_string()))?;
 
     terminal::disable_raw_mode()?;
 
     Ok(())
 }
 
-fn draw_devices(spusb: &SystemProfile, print_settings: &PrintSettings) -> Result<()> {
+fn draw_devices_buffered(
+    spusb: &SystemProfile,
+    print_settings: &PrintSettings,
+    display_data: &mut DisplayData,
+) -> Result<()> {
+    // use a Vec<u8> buffer instead of stdout
+    // so we can scroll the output with offset
+    display_data.buffer.clear();
+    let mut dw = DisplayWriter::new(&mut display_data.buffer);
+    dw.set_raw_mode(true);
+    dw.print_sp_usb(spusb, print_settings);
+
+    Ok(())
+}
+
+fn draw_footer<W: Write>(writer: &mut W, print_settings: &PrintSettings) -> Result<()> {
+    let (term_width, term_height) = terminal::size().unwrap_or((80, 24));
+
+    // construct footer message showing *toggle outcome* (what will happen on key press)
+    let verbosity = if print_settings.verbosity == 3 {
+        String::from("0")
+    } else {
+        (print_settings.verbosity + 1).to_string()
+    };
+    let footer = format!(
+        " [q] Quit  [v] Verbosity (→ {})  [t] Tree (→ {})  [h] Headings (→ {})  [m] More (→ {})  [d] Decimal (→ {}) ",
+        verbosity,
+        if print_settings.tree { "Off" } else { "On" },
+        if print_settings.headings { "Off" } else { "On" },
+        if print_settings.more { "Off" } else { "On" },
+        if print_settings.decimal { "Off" } else { "On" }
+    );
+
+    // move cursor to last row
+    execute!(
+        writer,
+        cursor::MoveTo(0, term_height - 1),
+        SetForegroundColor(Color::Black),
+        SetBackgroundColor(Color::Green),
+        Print(format!("{:<width$}", footer, width = term_width as usize)),
+        ResetColor
+    )?;
+
+    Ok(())
+}
+
+fn draw(display_data: &mut DisplayData, print_settings: &PrintSettings) -> Result<()> {
     let mut stdout = stdout();
+    let (_, term_height) = terminal::size().unwrap_or((80, 24));
+    let footer_height = 1;
+    let available_rows = term_height.saturating_sub(footer_height);
+
     execute!(
         stdout,
         cursor::MoveTo(0, 0),
@@ -155,12 +255,27 @@ fn draw_devices(spusb: &SystemProfile, print_settings: &PrintSettings) -> Result
     )
     .map_err(|e| Error::new(ErrorKind::Other("crossterm"), &e.to_string()))?;
 
-    // TODO change color based on event in print? or post print?
+    // convert buffer to string and split into lines
+    let output = String::from_utf8_lossy(&display_data.buffer);
+    let lines: Vec<String> = output.lines().map(|line| line.to_string()).collect();
+
+    display_data.max_offset = lines.len().saturating_sub(available_rows as usize);
+    // clamp ensures if output contracts fully scrolled, one doesn't have to *overscroll* back
+    display_data.scroll_offset = display_data.scroll_offset.min(display_data.max_offset);
+
+    // print the visible portion of the buffer
+    for line in lines
+        .iter()
+        .skip(display_data.scroll_offset)
+        .take(available_rows as usize)
+    {
+        write!(stdout, "{}\n\r", line)?;
+    }
+
     // status bar with key bindings
-    // header bar
-    let mut dw = DisplayWriter::new(&mut stdout);
-    dw.set_raw_mode(true);
-    dw.print_sp_usb(spusb, print_settings);
+    draw_footer(&mut stdout, print_settings)?;
+
+    stdout.flush()?;
 
     Ok(())
 }
